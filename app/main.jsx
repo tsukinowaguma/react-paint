5d399d9a5d239549cd55e503c1fc39db1d3b039f
tsukinowaguma <554969905@qq.com>
2017-02-27 02:12:02 +0800

    first_commit

---
+++ app/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Graffiti from './components/Graffiti';
+
+ReactDOM.render(
+  <Graffiti width={600} height={382} imgUrl="./components/img/b2834e104ebcfb3cba5955055a6a4cce.jpg" />,
+  document.body.appendChild(document.createElement('div'))
+);
