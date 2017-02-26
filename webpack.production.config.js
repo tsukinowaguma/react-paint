ec5498e6b5e40fbff0816ede3516d2c7b4837367
tsukinowaguma <554969905@qq.com>
2017-02-27 02:36:04 +0800

    README-update

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -17,7 +17,8 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.css$/, include: path.resolve(__dirname, 'app'), loader: 'style-loader!css-loader' },
-      { test: /\.js[x]?$/, include: path.resolve(__dirname, 'app'), exclude: /node_modules/, loader: 'babel-loader' }
+      { test: /\.js[x]?$/, include: path.resolve(__dirname, 'app'), exclude: /node_modules/, loader: 'babel-loader' },
+      { test:/\.(png|jpg)$/, include: path.resolve(__dirname, 'app'), loader:"url-loader?limit=8192&name=img/[hash:8].[name].[ext]}
     ]
   },
   resolve: {
